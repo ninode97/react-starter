c59f8e7aa19cd9ecf4ea834a705a84393cbcb394
ninode97 <lukas.pabijonavicius@gmail.com>
2021-10-16 15:20:47 +0300

    fix: remove firebase

--- src/components/domain/auth/SignOutButton.tsx
+++ src/components/domain/auth/SignOutButton.tsx
@@ -1,11 +1,11 @@
-import { useAuth } from "~/lib/firebase";
+
 
 type Props = {};
 
 export const SignOutButton = (props: Props) => {
   const handleClick = () => {
-    const auth = useAuth();
-    auth.signOut();
+    // const auth = useAuth();
+    // auth.signOut();
   };
 
   return (
